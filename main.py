129c938d7fcc00e2b84bab3b66f1539b08fed9a9
Charles Burgess <consulting@cvburgess.com>
2013-12-08 08:39:45 -0500

    create words and categories

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # Hangman by Charles Burgess
+
+"""
+
+*docs to go here*
+
+"""
+
+# Words and Categories
+
+foods = ["apple", "banana", "chocolate", "doughnut", "edamame", "gelato",
+         "hamburger", "icing", "jello", "mushroom", "papaya", "quiche",
+         "raspberry", "steak", "tangerine", "watermelon", "yams"]
+
+animals = ["ant", "bear", "cougar", "elephant", "flamingo", "gorilla",
+           "hedgehog", "kangaroo", "leopard", "mongoose", "narwhal",
+           "octopus", "platapus", "ringtail", "snake", "tyrannosaurus",
+           "walarus", "yeti", "zebra"]
+
+cities = ["atlanta", "baltimore", "chicago", "denver", "houston",
+          "minneapolis", "sacramento", "tampa", "vegas"]
+
+categories = {"animals": animals, "foods": foods, "cities": cities}
